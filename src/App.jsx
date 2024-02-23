ab52ce9451f0462ec3d59542d5063ed4365b943c
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2024-02-23 18:52:38 -0300

    pistola valhalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import pistola from './assets/collection.png'
+import card from './assets/card-notslot.png'
+import resizer from './assets/resize.png'
+import headerLine from './assets/header-line.png'
+import closeCardButton from './assets/close-icon.png'
 import './App.css'
 
 function App() {
   
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="info-card">
+        <div className="item-info-card main">
+          <div className="item-info-card-frame">
+            <div 
+              className="item-info-card-head"
+              style={{backgroundImage: "url(" + headerLine + ")"}}
+              >
+                <div className="collections-title">Collections</div>
+                <img src={closeCardButton} alt="" className="close-card-button" />
+              </div>
+            <div className="item-info-card-content">
+              <img src={pistola} alt="" className="item-image" />
+              <div className="item-info-card-content-right">
+                <div className="item-info-card-title"><span>Pistola Valhalla</span></div>
+                <div className="item-info-card-content-attr">
+                  <p>Pistola criada exclusivamente para membros iniciantes do Grupo do Éden.</p>
+                  <div className="divider">-----------------</div>
+                  <p className="blue">AGI +2. DES +2</p>
+                  <div className="divider">-----------------</div>
+                  <p>A cada 9 níveis base:</p>
+                  <p className="blue">ATQ +10</p>
+                  <div className="divider">-----------------</div>
+                  <p>A cada 18 níveis base:</p>
+                  <p className="blue">Dano de [Desperado] +3%.</p>
+                  <p className="blue">Velocidade de ataque +1%.</p>
+                  <div className="divider">-----------------</div>
+                  <p>Classe: <span className="purple">Pistola</span></p>
+                  <p>Forca de Ataque: <span className="red">15</span></p>
+                  <p>Peso: <span className="green">0</span></p>
+                  <p>Nível da arma: <span className="green">2</span></p>
+                  <p>Nível necessário: <span className="green">20</span></p>
+                  <p>Classes que utilizam: <span className="purple">Justiceiros</span></p>
+                  <p>Visualizar no Database: <a href="https://www.ragnatales.com.br/db/items/17965" className="link" target="_blank">[ID: 17965]</a></p>
+                </div>
+              </div>
+              
+            </div>
+            <img src={resizer} alt="" className="resizer" />
+          </div>
+        </div>
+        <div className="item-info-card add">
+          <div className="item-info-card-frame">
+            <p>ATQ +26</p>
+          </div>
+        </div>
+        <div className="item-info-card add">
+          <div className="item-info-card-frame">
+            <p>Dano Físico contra a raça Bruto +17%</p>
+          </div>
+        </div>
+        <div className="item-info-card add">
+          <div className="item-info-card-frame">
+            <div className="cards">
+              <img src={card} alt="" />
+              <img src={card} alt="" />
+              <img src={card} alt="" />
+              <img src={card} alt="" />
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <MyButton />
-        <MyButton />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-function MyButton() {
-  const [count, setCount] = useState(0)
-  function handleCLick() {
-    setCount(count + 1);
-  }
-  return (
-    <button onClick={handleCLick}>
-      Clicked {count} times
-    </button>
-  );
-}
 
 export default App
