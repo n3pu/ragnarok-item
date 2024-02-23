62a25751ec92d86e97de45795e070d8856832a28
Ewerton Nepomuceno <ewerton.gn@gmail.com>
2024-02-22 23:31:42 -0300

    Add Vite

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
@@ -18,9 +17,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <MyButton />
+        <MyButton />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -30,6 +28,18 @@ function App() {
       </p>
     </>
   )
+}
+
+function MyButton() {
+  const [count, setCount] = useState(0)
+  function handleCLick() {
+    setCount(count + 1);
+  }
+  return (
+    <button onClick={handleCLick}>
+      Clicked {count} times
+    </button>
+  );
 }
 
 export default App
